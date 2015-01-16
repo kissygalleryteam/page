423bfecda044da04b1087e96f5cfa598c7d3e375
kissyteam <tiehang.lth@taobao.com>
2015-01-16 14:15:07 +0800

    sync daily version 2.0.5

---
+++ 2.0.5/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2015-01-16 2:21:09 PM*/
+KISSY.config("modules",{"kg/page/2.0.5/index":{requires:["node","rich-base","event","uri"]}});
---
+++ 2.0.5/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/page/2.0.5/index': { requires: ['node', 'rich-base', 'event', 'uri']} 
+});
