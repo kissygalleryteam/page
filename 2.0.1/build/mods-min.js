c6f48132e6cb8e7f5de19c889005b6bdea46c1fd
kissyteam <tiehang.lth@taobao.com>
2015-01-20 09:43:39 +0800

    sync daily branch 0.2.5

--- 2.0.1/build/mods-min.js
+++
@@ -1,2 +0,0 @@
-/*!build time : 2014-08-07 11:57:15 AM*/
-KISSY.config("modules",{"kissy-kg/page/2.0.0/index":{requires:["node","rich-base","event","uri"]}});
--- doc/build/mods-min.js
+++
@@ -1,2 +0,0 @@
-/*!build time : 2015-01-16 2:21:09 PM*/
-KISSY.config("modules",{"kg/page/2.0.5/index":{requires:["node","rich-base","event","uri"]}});
